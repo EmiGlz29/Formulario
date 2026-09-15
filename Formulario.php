0475b6464fe32e592ced98f5e9b2944b12cfa839
EmiGlz29 <emiliano.gonzalez9141@alumnos.udg.mx>
2026-09-15 12:25:09 -0600

    Se cambia la direccion de la carpeta a www de laragon y se añaden los campos de name para pasar la informacion

--- Formulario.php
+++ Formulario.php
@@ -9,18 +9,18 @@
         <h1 class="text-center bg-danger text-white">Formulario</h1>
         <form action="procesar_formulario.php" method="post">
             <div class="form-floating mb-3">
-            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-            <label for="floatingInput">Email</label>
+                <input type="email" class="form-control" id="floatingInput" name="correo" placeholder="name@example.com">
+                <label for="floatingInput">Email</label>
             </div>
             <div class="form-floating">
-            <input type="Name" class="form-control" id="floatingName" placeholder="Name">
-            <label for="floatingName">Name</label>
-</div>
+                <input type="text" class="form-control" id="floatingName" name="nombre" placeholder="Name">
+                <label for="floatingName">Name</label>
+            </div>
             <br>
-            <label for="Fecha de nacimiento">Fecha de nacimiento:</label>
-            <input type="date" id="Fecha de nacimiento" name="Fecha de nacimiento" required>
-            <br>
-        <button type="submit" class="btn btn-primary">Enviar</button>
+            <label for="fecha_nacimiento">Fecha de nacimiento:</label>
+            <input type="date" id="fecha_nacimiento" name="fecha_nacimiento" required>
+            <br><br>
+            <button type="submit" class="btn btn-primary">Enviar</button>
         </form>
     </body>
 </html>
